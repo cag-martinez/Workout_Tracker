7f6fc8a30c36762f90b07657b1b4d11f4b2b88d0
Christian Gomez Martinez <gomez9289@gmail.com>
2020-06-23 23:56:40 -0400

    updated workouts js

---
+++ models/Workout.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema =new Schema({
+
+});
+
+
+const User = mongoose.model("User", WorkoutSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require("express");
+const mongojs = require("mongojs");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
-const PORT = 3000;
+//const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+const db = require("./model");
 const app = express();
 
 app.use(morgan("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethods", { useNewUrlParser: true });
+
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .then(dbUser => {
+        res.json(dbUser);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
 
 app.listen(PORT, () => console.log(`server listening at http:localhost:${PORT}/`))
