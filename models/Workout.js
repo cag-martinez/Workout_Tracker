1c08c563c728d98d554af7a0fa0ecce0b81c7d20
Christian Gomez Martinez <gomez9289@gmail.com>
2020-06-24 19:15:12 -0400

    workout schema

--- models/Workout.js
+++ models/Workout.js
@@ -3,6 +3,50 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema =new Schema({
+    day: {
+        type: Date,
+        default: date.now()
+    },
+    excersises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                require: true
+            },
+            name: {
+                type: String,
+                trim: true, 
+                require: true
+            },
+            duration: {
+                type: Number,
+                trim: true,
+                require: true
+            },
+            wight: {
+                type: Number,
+                trim: false,
+                require: true
+            },
+            reps: {
+                type: Number,
+                trim: false,
+                require: true
+            },
+            sets: {
+                type: Number,
+                trim: false,
+                require: true
+            },
+            distance: {
+                type: Number,
+                trim: false,
+                require: true
+            },
+        }
+    ]
+
 
 });
 
